9706b1bc8299f0667c8bdab279d03f021a18d8f2
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2022-08-25 13:47:23 +0200

    Implement datasets

--- ariel.py
+++ ariel.py
@@ -9,17 +9,18 @@ import torch
 from torch import nn
 from torch import optim
 from torch.nn import functional as F
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader, Dataset
 import wandb
 
 
 N_WAVES = 52
+N_AUXILLARY = 9
 N_TARGETS = 6
 QUARTILES = [0.16, 0.5, 0.84]
 N = 21987
 HYPERPARAMETER_DEFAULTS = dict(
         batch_size=256,
-        learning_rate=0.0002,
+        learning_rate=0.0001,
         patience=2048,
         n_hiddens=7,
         n_neurons=128,
@@ -28,11 +29,31 @@ HYPERPARAMETER_DEFAULTS = dict(
 
 def read_spectra(path="data/train/spectra.hdf5", n=N):
     with h5py.File(path, "r") as file:
-        X = torch.zeros((n, 1, 52), dtype=torch.float64)
+        spectra = torch.zeros((4, n, 52))
         for i in range(n):
             key = "Planet_" + str(i)
-            X[i, 0] = torch.from_numpy(file[key]["instrument_spectrum"][:])
-    return X
+            spectra[0, i] = torch.from_numpy(file[key]["instrument_wlgrid"][:])
+            spectra[1, i] = torch.from_numpy(file[key]["instrument_spectrum"][:])
+            spectra[2, i] = torch.from_numpy(file[key]["instrument_noise"][:])
+            spectra[3, i] = torch.from_numpy(file[key]["instrument_width"][:])
+    return spectra
+
+
+def read_traces(path="data/train/ground_truth/traces.hdf5", n=N):
+    with h5py.File(path, "r") as file:
+        mean = torch.zeros((n, 6))
+        var = torch.zeros((n, 6))
+        for i in range(n):
+            key = "Planet_" + str(i)
+            tracedata = file[key]["tracedata"][:]
+            weights = file[key]["weights"][:]
+            mean[i] = torch.from_numpy(tracedata.T @ weights)
+    return mean
+
+
+def read_auxillary_table(path="data/train/auxillary_table.csv", n=N):
+    auxillary = pd.read_csv(path, index_col="planet_ID", nrows=n)
+    return torch.from_numpy(auxillary.values).float()
 
 
 def read_fm_parameter_table(path="data/train/ground_truth/fm_parameter_table.csv", n=N):
@@ -49,14 +70,6 @@ def read_quartiles_table(path="data/train/ground_truth/quartiles_table.csv", n=N
         for j, target in enumerate(targets):
             X[i, :, j] = df.loc[:, target + '_' + quartile]
     return X
-
-
-def standardise(tensor, mean, std):
-    return (tensor - mean) / std
-
-
-def unstandardise(tensor, mean, std):
-    return tensor * std + mean
 
 
 def light_score(quartiles, quartiles_pred):
@@ -89,6 +102,46 @@ def regular_track_format(traces, weights=None, filename="data/regular_track.hdf5
             grp.create_dataset('weights', data=weight)
 
 
+def standardise(tensor, mean, std):
+    return (tensor - mean) / std
+
+
+class SpectraDataset(Dataset):
+    def __init__(self, X, Y, quartiles, X_mean, X_std):
+        self.X = X
+        self.Y = Y
+        self.quartiles = quartiles
+        self.X_mean = X_mean
+        self.X_std = X_std
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, idx):
+        x, y = self.X[idx], self.Y[idx]
+        x = (x - self.X_mean) / self.X_std
+        return x, y
+
+
+class NoisySpectraDataset(Dataset):
+    def __init__(self, X, noise, Y, quartiles, X_mean, X_std):
+        self.X = X
+        self.noise = noise
+        self.Y = Y
+        self.quartiles = quartiles
+        self.X_mean = X_mean
+        self.X_std = X_std
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, idx):
+        x, y = self.X[idx], self.Y[idx]
+        x = x + torch.normal(torch.tensor(0.0, device=torch.device("cuda")), noise_train[idx])
+        x = (x - self.X_mean) / self.X_std
+        return x, y
+
+
 class Model(nn.Module):
     def __init__(self, config):
         super(Model, self).__init__()
@@ -104,7 +157,6 @@ class Model(nn.Module):
             self.cuda()
 
     def forward(self, X):
-        X = torch.flatten(X, 1)
         X = F.relu(self.input(X))
         for linear in self.linears:
             X = F.relu(linear(X))
@@ -114,29 +166,30 @@ class Model(nn.Module):
         return mean, var
 
     def loss(self, Y_pred, Y):
-        mean, var = Y_pred[0], Y_pred[1]
-        return torch.mean(0.5 * torch.log(var) + ((Y - mean).square()) / (2 * var))
+        mean, var = Y_pred
+        return torch.mean(0.5 * torch.log(var) + 0.5 * (Y - mean).square() / var)
 
-    def evaluate(self, dataset):
-        X, quartiles = dataset
-        mean, var = self(X)
+    @torch.no_grad()
+    def predict(self, dataset, batch_size=2048):
+        dataloader = DataLoader(dataset, batch_size=batch_size)
+        mean, var = list(zip(*[self(X_batch) for X_batch, _ in dataloader]))
+        mean, var = torch.concat(mean), torch.concat(var)
+        return mean, var
+
+    def evaluate(self, Y_pred, dataset):
+        mean, var = Y_pred
         mean, var = mean.cpu().numpy(), var.cpu().numpy()
         std = np.sqrt(var)
         quartiles_pred = np.stack([norm.ppf(quartile, loc=mean, scale=std) for quartile in QUARTILES])
-        return light_score(quartiles, quartiles_pred)
+        return light_score(dataset.quartiles, quartiles_pred)
 
 
 def train(Model, trainset, validset, config):
-    X_train, Y_train, quartiles_train = trainset
-    X_valid, Y_valid, quartiles_valid = validset
     with wandb.init(config=config, project="ariel-data-challenge"):
         config = wandb.config
         model = Model(config)
         wandb.watch(model)
-        trainloader = DataLoader(
-                TensorDataset(X_train, Y_train),
-                batch_size=config["batch_size"],
-                shuffle=True)
+        trainloader = DataLoader(trainset, batch_size=config["batch_size"], shuffle=True)
         optimiser = optim.Adam(
                 model.parameters(),
                 lr=config["learning_rate"],
@@ -151,11 +204,12 @@ def train(Model, trainset, validset, config):
                 loss.backward()
                 optimiser.step()
             model.eval()
-            with torch.no_grad():
-                loss_train = model.loss(model(X_train), Y_train).item()
-                loss_valid = model.loss(model(X_valid), Y_valid).item()
-                score_train = model.evaluate((X_train, quartiles_train))
-                score_valid = model.evaluate((X_valid, quartiles_valid))
+            output_train = model.predict(trainset)
+            output_valid = model.predict(validset)
+            loss_train = model.loss(output_train, trainset.Y).item()
+            loss_valid = model.loss(output_valid, validset.Y).item()
+            score_train = model.evaluate(output_train, trainset)
+            score_valid = model.evaluate(output_valid, validset)
             if score_valid > score_valid_best:
                 i = 0
                 score_valid_best = score_valid
@@ -163,6 +217,7 @@ def train(Model, trainset, validset, config):
                 loss_train_at_best = loss_train
                 loss_valid_at_best = loss_valid
                 model_state_best = deepcopy(model.state_dict())
+                torch.save(model_state_best, f"models/{wandb.run.name}.pt")
             else:
                 i += 1
             wandb.log({
@@ -176,14 +231,16 @@ def train(Model, trainset, validset, config):
             wandb.run.summary["light_score_train"] = score_train_at_best
             wandb.run.summary["light_score_valid"] = score_valid_best
         model.load_state_dict(model_state_best)
-        torch.save(model_state_best, f"models/{wandb.run.name}.pt")
+        return model
 
 
 if __name__ == "__main__":
-    # get data
-    X = read_spectra()
+    spectra = read_spectra()
+    X = spectra[1]
+    noise = spectra[2]
+    auxillary = read_auxillary_table()
     quartiles = read_quartiles_table()
-    Y = torch.from_numpy(quartiles[1])
+    Y = read_traces()
 
     # train and validation set split
     # TODO out-of-distribution split?
@@ -192,23 +249,27 @@ if __name__ == "__main__":
     idx_train = torch.zeros_like(ids, dtype=torch.bool)
     idx_train[ids_train] = True
     idx_valid = ~idx_train
+
     X_train, X_valid = X[idx_train], X[idx_valid]
+    noise_train, noise_valid = noise[idx_train], noise[idx_valid]
+    auxillary_train, auxillary_valid = auxillary[idx_train], auxillary[idx_valid]
     Y_train, Y_valid = Y[idx_train], Y[idx_valid]
     quartiles_train, quartiles_valid = quartiles[:, idx_train], quartiles[:, idx_valid]
 
     # data preparation
     X_train_mean, X_train_std = X_train.mean(), X_train.std()
-    X_train = standardise(X_train, X_train_mean, X_train_std)
-    X_valid = standardise(X_valid, X_train_mean, X_train_std)
-
-    X_train, X_valid = X_train.float(), X_valid.float()
-    Y_train, Y_valid = Y_train.float(), Y_valid.float()
+    auxillary_train_mean = auxillary_train.mean(dim=0)
+    auxillary_train_std = auxillary_train.std(dim=0)
+    auxillary_train = standardise(auxillary_train, auxillary_train_mean, auxillary_train_std)
+    auxillary_valid = standardise(auxillary_valid, auxillary_train_mean, auxillary_train_std)
 
     X_train, X_valid = X_train.cuda(), X_valid.cuda()
+    noise_train, noise_valid = noise_train.cuda(), noise_valid.cuda()
+    auxillary_train, auxillary_valid = auxillary_train.cuda(), auxillary_valid.cuda()
     Y_train, Y_valid = Y_train.cuda(), Y_valid.cuda()
 
-    train(
-            Model,
-            (X_train, Y_train, quartiles_train),
-            (X_valid, Y_valid, quartiles_valid),
-            HYPERPARAMETER_DEFAULTS)
+    trainset = NoisySpectraDataset(
+            X_train, noise_train, Y_train, quartiles_train, X_train_mean, X_train_std)
+    validset = SpectraDataset(X_valid, Y_valid, quartiles_valid, X_train_mean, X_train_std)
+
+    model = train(Model, trainset, validset, HYPERPARAMETER_DEFAULTS)
