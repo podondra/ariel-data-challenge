e07467994b2add97144f47f429630d41aeb5af1e
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2022-09-02 08:06:27 +0200

    Modify data loading

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Python 3.9.6 and CUDA 11.3.1.
 
 1. Implement full covariance matrix?
 1. Implement convolutional neural network.
+1. Early stopping on a correct metric: is light score the metric?
+1. Noisy spectra.
--- ariel.py
+++ ariel.py
@@ -15,17 +15,11 @@ from tqdm import tqdm
 import wandb
 
 
-DEFAULT_PRIOR_BOUNDS = np.array([
-    [0, 3000],    # T range
-    [-12, -2],    # 1. gas range
-    [-12, -2],    # 2. gas range
-    [-12, -2],    # 3. gas range
-    [-12, -2],    # 4. gas range
-    [-12, -2]])    # 5. gas range
+DEFAULT_PRIOR_BOUNDS = np.array([[0, -12, -12, -12, -12, -12], [3000, -2, -2, -2, -2, -2]])
 HYPERPARAMETER_DEFAULTS = dict(
         batch_size=256,
-        learning_rate=0.00001,
-        n_hiddens=5,
+        learning_rate=0.0015,
+        n_hiddens=4,
         n_neurons=2048,
         patience=2048)
 N = 21987
@@ -35,11 +29,12 @@ N_WAVES = 52
 QUARTILES = [0.16, 0.5, 0.84]
 
 
-def read_spectra(path="data/train/spectra.hdf5", n=N):
+def read_spectra(ids, path="data/train/spectra.hdf5"):
+    n = ids.shape[0]
     with h5py.File(path, "r") as file:
         spectra = torch.zeros((4, n, 52))
-        for i in range(n):
-            key = "Planet_" + str(i)
+        for i, identifier in enumerate(ids):
+            key = "Planet_" + str(identifier)
             spectra[0, i] = torch.from_numpy(file[key]["instrument_wlgrid"][:])
             spectra[1, i] = torch.from_numpy(file[key]["instrument_spectrum"][:])
             spectra[2, i] = torch.from_numpy(file[key]["instrument_noise"][:])
@@ -47,32 +42,33 @@ def read_spectra(path="data/train/spectra.hdf5", n=N):
     return spectra
 
 
-def read_traces(path="data/train/ground_truth/traces.hdf5", n=N):
+def read_traces(ids, path="data/train/ground_truth/traces.hdf5"):
+    n = ids.shape[0]
     with h5py.File(path, "r") as file:
         means = torch.zeros((n, 6))
         variances = torch.zeros((n, 6))
-        for i in range(n):
-            key = "Planet_" + str(i)
-            tracedata = file[key]["tracedata"][:]
+        for i, identifier in enumerate(ids):
+            key = "Planet_" + str(identifier)
+            trace = file[key]["tracedata"][:]
             weights = file[key]["weights"][:]
-            mean = tracedata.T @ weights
+            mean = trace.T @ weights
             means[i] = torch.from_numpy(mean)
-            variances[i] = torch.from_numpy(np.square(tracedata - mean).T @ weights)
+            variances[i] = torch.from_numpy(np.square(trace - mean).T @ weights)
     return means, variances
 
 
-def read_auxiliary_table(path="data/train/auxiliary_table.csv", n=N):
-    auxiliary = pd.read_csv(path, index_col="planet_ID", nrows=n)
-    return torch.from_numpy(auxiliary.values).float()
+def read_auxiliary_table(ids, path="data/train/auxiliary_table.csv"):
+    auxiliary = pd.read_csv(path, index_col="planet_ID")
+    return torch.from_numpy(auxiliary.loc[ids].values).float()
 
 
-def read_fm_parameter_table(path="data/train/ground_truth/fm_parameter_table.csv", n=N):
-    fm_parameter = pd.read_csv(path, index_col="planet_ID", nrows=n)
-    return torch.from_numpy(fm_parameter.values)
+def read_fm_parameter_table(ids, path="data/train/ground_truth/fm_parameter_table.csv"):
+    fm_parameter = pd.read_csv(path, index_col="planet_ID")
+    return torch.from_numpy(fm_parameter.loc[ids].values)
 
 
-def read_quartiles_table(path="data/train/ground_truth/quartiles_table.csv", n=N):
-    df = pd.read_csv(path, index_col="planet_ID", nrows=n)
+def read_quartiles_table(ids, path="data/train/ground_truth/quartiles_table.csv"):
+    df = pd.read_csv(path, index_col="planet_ID").loc[ids]
     quartiles = ["q1", "q2", "q3"]
     targets = ["T", "log_H2O", "log_CO2", "log_CH4", "log_CO", "log_NH3"]
     X = np.zeros((len(quartiles), df.shape[0], len(targets)))
@@ -83,7 +79,7 @@ def read_quartiles_table(path="data/train/ground_truth/quartiles_table.csv", n=N
 
 
 def light_score(quartiles, quartiles_pred):
-    return 100 * (10 - np.sqrt(((1 - quartiles_pred / quartiles) ** 2).mean()))
+    return 100 * (10 - np.sqrt(np.mean(np.square((quartiles - quartiles_pred) / quartiles))))
 
 
 def light_track_format(quartiles, filename="data/light_track.csv"):
@@ -98,31 +94,30 @@ def light_track_format(quartiles, filename="data/light_track.csv"):
     return df
 
 
-def normalise(array, bounds_matrix):
-    return (array - bounds_matrix[:, 0]) / (bounds_matrix[:, 1] - bounds_matrix[:, 0])
+def normalise(matrix, prior_bounds):
+    return (matrix - prior_bounds[0]) / (prior_bounds[1] - prior_bounds[0])
 
 
-def wasserstein(trace1, trace2, w1=None, w2=None, bounds_matrix=DEFAULT_PRIOR_BOUNDS):
-    w1 = ot.unif(len(trace1)) if w1 is None else w1
-    w2 = ot.unif(len(trace2)) if w2 is None else w2
-    trace1 = normalise(trace1, bounds_matrix)
-    trace2 = normalise(trace2, bounds_matrix)
+def wasserstein(trace1, trace2, w2, prior_bounds=DEFAULT_PRIOR_BOUNDS):
+    w1 = ot.unif(trace1.shape[0])
+    trace1 = normalise(trace1, prior_bounds)
+    trace2 = normalise(trace2, prior_bounds)
     M = ot.dist(trace1, trace2)
     M /= M.max()
     return ot.emd2(w1, w2, M, numItermax=100000)
 
 
-def regular_score(tracefile, traces_pred, ids):
+def regular_score(traces_pred, ids, tracefile="data/train/ground_truth/traces.hdf5"):
     # calculate the score for regular track from a predicted trace matrix (N X M X 6)
     # and a ground truth HDF5 file
     score = 0
     n = ids.shape[0]
     with h5py.File(tracefile, "r") as traces:
         for i, trace_pred in tqdm(zip(ids, traces_pred), total=n):
-            key = 'Planet_' + str(i.item())
-            tracedata = traces[key]['tracedata'][:]
-            weights = traces[key]['weights'][:]
-            score += wasserstein(trace_pred, tracedata, w2=weights)
+            key = "Planet_" + str(i)
+            trace = traces[key]["tracedata"][:]
+            weights = traces[key]["weights"][:]
+            score += wasserstein(trace_pred, trace, w2=weights)
     return 1000 * (1 - score / n)
 
 
@@ -135,9 +130,9 @@ def regular_track_format(traces, weights=None, filename="data/regular_track.hdf5
     with h5py.File(filename, "w") as tracefile:
         for i, (trace, weight) in enumerate(zip(traces, weights)):
             grp = tracefile.create_group("Planet_" + str(i))
-            grp.attrs['ID'] = i
-            grp.create_dataset('tracedata', data=trace)
-            grp.create_dataset('weights', data=weight)
+            grp.attrs["ID"] = i
+            grp.create_dataset("tracedata", data=trace)
+            grp.create_dataset("weights", data=weight)
 
 
 def standardise(tensor, mean, std):
@@ -296,45 +291,34 @@ def train(Model, trainset, validset, config):
         return model
 
 
-def get_datasets():
-    spectra = read_spectra()
-    X = spectra[1]
-    noise = spectra[2]
-    auxiliary = read_auxiliary_table()
-    quartiles = read_quartiles_table()
-    Y = read_traces()
-
-    # train and validation set split
-    # TODO out-of-distribution split?
-    ids = torch.arange(N)
-    ids_train, ids_valid = train_test_split(ids, train_size=0.8, random_state=36)
-    idx_train = torch.zeros_like(ids, dtype=torch.bool)
-    idx_train[ids_train] = True
-    idx_valid = ~idx_train
-
-    X_train, X_valid = X[idx_train], X[idx_valid]
-    noise_train, noise_valid = noise[idx_train], noise[idx_valid]
-    auxiliary_train, auxiliary_valid = auxiliary[idx_train], auxiliary[idx_valid]
-    Y_train, Y_valid = (Y[0][idx_train], Y[1][idx_train]), (Y[0][idx_valid], Y[1][idx_valid])
-    quartiles_train, quartiles_valid = quartiles[:, idx_train], quartiles[:, idx_valid]
-
-    X_train_mean, X_train_std = X_train.mean(), X_train.std()
-    auxiliary_train_mean = auxiliary_train.mean(dim=0)
-    auxiliary_train_std = auxiliary_train.std(dim=0)
-
-    trainset = SpectraDataset(
-            ids_train,
-            X_train, X_train_mean, X_train_std,
-            auxiliary_train, auxiliary_train_mean, auxiliary_train_std,
-            Y_train, quartiles_train)
-    validset = SpectraDataset(
-            ids_valid,
-            X_valid, X_train_mean, X_train_std,
-            auxiliary_valid, auxiliary_train_mean, auxiliary_train_std,
-            Y_valid, quartiles_valid)
-    return trainset, validset
+def get_dataset(ids, X_train_mean=None, X_train_std=None, auxiliary_train_mean=None, auxiliary_train_std=None):
+    spectra = read_spectra(ids)
+    X, noise = spectra[1], spectra[2]
+    auxiliary = read_auxiliary_table(ids)
+    quartiles = read_quartiles_table(ids)
+    Y = read_traces(ids)
+    return SpectraDataset(
+            ids,
+            X,
+            X.mean() if X_train_mean is None else X_train_mean,
+            X.std() if X_train_std is None else X_train_std,
+            auxiliary,
+            auxiliary.mean(dim=0) if auxiliary_train_mean is None else auxiliary_train_mean,
+            auxiliary.std(dim=0) if auxiliary_train_std is None else auxiliary_train_std,
+            Y,
+            quartiles)
 
 
 if __name__ == "__main__":
-    trainset, validset = get_datasets()
+    # train and validation set split
+    # TODO out-of-distribution split?
+    ids = np.arange(N)
+    ids_train, ids_valid = train_test_split(ids, train_size=0.8, random_state=36)
+    trainset = get_dataset(ids_train)
+    validset = get_dataset(
+            ids_valid,
+            trainset.X_train_mean,
+            trainset.X_train_std,
+            trainset.auxiliary_train_mean,
+            trainset.auxiliary_train_std)
     model = train(Model, trainset, validset, HYPERPARAMETER_DEFAULTS)
